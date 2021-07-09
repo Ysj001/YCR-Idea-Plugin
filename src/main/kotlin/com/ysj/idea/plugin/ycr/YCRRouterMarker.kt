1e8d61bece544d791dc2ba655b3aa044a51e7d28
Ysj001 <543501451@qq.com>
2021-07-10 00:54:06 +0800

    <完善> 路由定位功能

--- src/main/kotlin/com/ysj/idea/plugin/ycr/YCRRouterMarker.kt
+++ src/main/kotlin/com/ysj/idea/plugin/ycr/YCRRouterMarker.kt
@@ -5,7 +5,6 @@ import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProviderDescriptor
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.editor.markup.GutterIconRenderer
-import com.intellij.openapi.ui.Messages
 import com.intellij.pom.Navigatable
 import com.intellij.psi.*
 import com.intellij.psi.search.GlobalSearchScope
@@ -13,7 +12,6 @@ import com.intellij.psi.search.searches.AnnotatedMembersSearch
 import com.intellij.util.containers.isNullOrEmpty
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.plainContent
-import java.awt.event.MouseEvent
 
 /**
  * 用于定位 YCR 的 build("/aaa/bbb") 对应的 @Route(path = "/aaa/bbb") 的标记类
@@ -25,40 +23,16 @@ class YCRRouterMarker : LineMarkerProviderDescriptor() {
 
     override fun getName() = "YCR"
 
-    override fun getLineMarkerInfo(element: PsiElement) =
-        if (element is KtElement) kotlinProcess(element) else javaProcess(element)
-
-    private fun javaProcess(element: PsiElement): LineMarkerInfo<*>? {
-        if (element !is PsiCallExpression) return null
-        val method = element.resolveMethod() ?: return null
-        val parent = method.parent
-        if (method.name != "build" || parent !is PsiClass) return null
-        if (parent.name != SDK_NAME && parent.supers.find { it.name != SDK_NAME } != null) return null
-        val args = element.argumentList?.expressions
-        if (args.isNullOrEmpty()) return null
-        val routePath = args[0] as? PsiLiteralValue ?: return null
-        searchTarget(element, routePath.value.toString()) ?: return null
+    override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<out PsiElement>? {
+        searchTarget(element) ?: return null
         return markerInfo(element) { _, elm ->
-            // GutterIconNavigationHandler 这玩意会复用？所以重新 searchTarget
-            searchTarget(elm, routePath.value.toString())?.navigate(true)
+            // GutterIconNavigationHandler 这玩意会复用？
+            searchTarget(elm)?.navigate(true)
         }
     }
 
-    private fun kotlinProcess(element: KtElement): LineMarkerInfo<*>? {
-        if (element !is KtNameReferenceExpression) return null
-        if (element.getReferencedName() != "build") return null
-        val parent = (element as PsiElement).parent
-        if (parent !is KtCallExpression) return null
-        val args = parent.valueArguments
-        if (args.isNullOrEmpty()) return null
-        val routePath = (args[0].getArgumentExpression() as? KtStringTemplateExpression)?.plainContent ?: return null
-        searchTarget(element, routePath) ?: return null
-        return markerInfo(element) { _, elm ->
-            searchTarget(elm, routePath)?.navigate(true)
-        }
-    }
-
-    private fun searchTarget(element: PsiElement, routePath: String): Navigatable? {
+    private fun searchTarget(element: PsiElement): Navigatable? {
+        val routePath = matchRoutePath(element) ?: return null
         val project = element.project
         val allScope = GlobalSearchScope.allScope(project)
         val routeAnnotation = JavaPsiFacade.getInstance(project).findClass(ROUTE_ANNOTATION_NAME, allScope)
@@ -72,6 +46,28 @@ class YCRRouterMarker : LineMarkerProviderDescriptor() {
                     group.value.toString().run { if (isEmpty()) path.value else "$this/${path.value}" }
                 }
             }
+    }
+
+    private fun matchRoutePath(element: PsiElement): String? {
+        if (element is KtElement) return matchRoutePath(element as KtElement)
+        if (element !is PsiCallExpression) return null
+        val method = element.resolveMethod() ?: return null
+        val parent = method.parent
+        if (method.name != "build" || parent !is PsiClass) return null
+        if (parent.name != SDK_NAME && parent.supers.find { it.name != SDK_NAME } != null) return null
+        val args = element.argumentList?.expressions
+        if (args.isNullOrEmpty()) return null
+        return (args[0] as? PsiLiteralValue)?.value?.toString() ?: return null
+    }
+
+    private fun matchRoutePath(element: KtElement): String? {
+        if (element !is KtNameReferenceExpression) return null
+        if (element.getReferencedName() != "build") return null
+        val parent = (element as PsiElement).parent
+        if (parent !is KtCallExpression) return null
+        val args = parent.valueArguments
+        if (args.isNullOrEmpty()) return null
+        return (args[0].getArgumentExpression() as? KtStringTemplateExpression)?.plainContent ?: return null
     }
 
     private fun markerInfo(
