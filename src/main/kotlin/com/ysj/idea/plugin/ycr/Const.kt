3f801f787892ce50274f24ac3f591ab9cf13e2e6
Ysj001 <543501451@qq.com>
2021-07-09 23:29:18 +0800

    <新增> 常量定义文件

---
+++ src/main/kotlin/com/ysj/idea/plugin/ycr/Const.kt
@@ -0,0 +1,16 @@
+package com.ysj.idea.plugin.ycr
+
+/*
+ * 定义常量
+ *
+ * @author Ysj
+ * Create time: 2021/7/8
+ */
+
+const val PLUGIN_HELP_URL = "https://github.com/Ysj001/YCR-Idea-Plugin"
+
+const val SDK_NAME = "YCR"
+
+const val SDK_PACKAGE_NAME = "com.ysj.lib.ycr"
+
+const val ROUTE_ANNOTATION_NAME = "$SDK_PACKAGE_NAME.annotation.Route"
