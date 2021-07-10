66f24f2b5bff560a0191b37631a4c2ffd85505d1
Ysj001 <543501451@qq.com>
2021-07-10 19:07:11 +0800

    <新增> build 方法 path 内容提示和自动补全功能

---
+++ src/main/kotlin/com/ysj/idea/plugin/ycr/RoutePathContributor.kt
@@ -0,0 +1,82 @@
+package com.ysj.idea.plugin.ycr
+
+import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.lang.Language
+import com.intellij.psi.*
+import com.intellij.psi.util.parents
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtStringTemplateExpression
+import org.jetbrains.kotlin.psi.KtValueArgument
+import org.jetbrains.kotlin.psi.psiUtil.plainContent
+
+/**
+ * build 方法 path 内容提示和自动补全功能
+ *
+ * - Windows: Ctrl+Shift+Space
+ * - MacOS: ⌃⇧Space
+ *
+ * [CompletionContributor 文档](https://plugins.jetbrains.com/docs/intellij/completion-contributor.html#register-the-completion-contributor)
+ *
+ * @author Ysj
+ * Create time: 2021/7/9
+ */
+class RoutePathContributor : CompletionContributor() {
+
+    override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
+        val position = parameters.originalPosition ?: return
+        result.addAllElements(
+            when (position.language) {
+                Language.findLanguageByID("JAVA") -> {
+                    val element = position.parents.find { it is PsiMethodCallExpression }
+                    val args = element?.buildArgs()
+                    if (args.isNullOrEmpty()) return
+                    val inputValue = args[0].cast<PsiLiteralValue>()?.value?.toString()
+                    element.results(inputValue) { context, item ->
+                        if (inputValue.isNullOrEmpty()) return@results
+                        element.buildArgs()?.get(0)?.run {
+                            val length = text.replace(item.lookupString, "").length
+                            val start = textRange.startOffset
+                            context.document.deleteString(start + 1, start + length - 1)
+                        }
+                    } ?: return
+                }
+                Language.findLanguageByID("kotlin") -> {
+                    val element = position.parents.find { it is KtCallExpression } ?: return
+                    val args = element.cast<KtElement>()?.buildArgs()
+                    if (args.isNullOrEmpty()) return
+                    val inputValue = args[0].getArgumentExpression().cast<KtStringTemplateExpression>()?.plainContent
+                    element.results(inputValue) { context, item ->
+                        if (inputValue.isNullOrEmpty()) return@results
+                        element.cast<KtElement>()?.buildArgs()?.get(0)?.cast<PsiElement>()?.run {
+                            val length = text.replace(item.lookupString, "").length
+                            val start = textRange.startOffset
+                            context.document.deleteString(start + 1, start + length - 1)
+                        }
+                    } ?: return
+                }
+                else -> return
+            }
+        )
+    }
+
+    private fun PsiElement.results(
+        inputValue: String?,
+        insertHandler: InsertHandler<LookupElement>,
+    ) = project.findRouteAnnotations()?.mapNotNull {
+        val annotation = it.getAnnotation(ROUTE_ANNOTATION_NAME) ?: return@mapNotNull null
+        val path = annotation.findAttributeValue("path")
+        val group = annotation.findAttributeValue("group")
+        if (path !is PsiLiteralValue || group !is PsiLiteralValue) return@mapNotNull null
+        val targetPath = group.value.toString().run {
+            if (isEmpty()) path.value.toString() else "$this/${path.value}"
+        }
+        LookupElementBuilder.create(if (inputValue == null) "\"$targetPath\"" else targetPath)
+            .withTailText(" " + (annotation.parent.parent.cast<PsiClass>())?.qualifiedName)
+            .withTypeText("YCR")
+            .withInsertHandler(insertHandler)
+    }
+
+}
