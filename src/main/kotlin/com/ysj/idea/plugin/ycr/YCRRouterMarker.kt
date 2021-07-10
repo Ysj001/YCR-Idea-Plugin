363fb34fca3d4d2fb8adfbaa8e33be4be1eec906
Ysj001 <543501451@qq.com>
2021-07-10 19:05:58 +0800

    <完善> 路由目标标记功能

--- src/main/kotlin/com/ysj/idea/plugin/ycr/YCRRouterMarker.kt
+++ src/main/kotlin/com/ysj/idea/plugin/ycr/YCRRouterMarker.kt
@@ -7,9 +7,6 @@ import com.intellij.icons.AllIcons
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.pom.Navigatable
 import com.intellij.psi.*
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.search.searches.AnnotatedMembersSearch
-import com.intellij.util.containers.isNullOrEmpty
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.plainContent
 
@@ -33,42 +30,22 @@ class YCRRouterMarker : LineMarkerProviderDescriptor() {
 
     private fun searchTarget(element: PsiElement): Navigatable? {
         val routePath = matchRoutePath(element) ?: return null
-        val project = element.project
-        val allScope = GlobalSearchScope.allScope(project)
-        val routeAnnotation = JavaPsiFacade.getInstance(project).findClass(ROUTE_ANNOTATION_NAME, allScope)
-        return AnnotatedMembersSearch.search(routeAnnotation ?: return null, allScope)
-            .findAll()
-            .find { mb ->
-                routePath in mb.annotations.mapNotNull { ann ->
-                    val path = ann.findAttributeValue("path")
-                    val group = ann.findAttributeValue("group")
-                    if (path !is PsiLiteralValue || group !is PsiLiteralValue) return@mapNotNull null
-                    group.value.toString().run { if (isEmpty()) path.value else "$this/${path.value}" }
-                }
+        return element.project.findRouteAnnotations()?.find { mb ->
+            routePath in mb.annotations.mapNotNull { ann ->
+                val path = ann.findAttributeValue("path")
+                val group = ann.findAttributeValue("group")
+                if (path !is PsiLiteralValue || group !is PsiLiteralValue) return@mapNotNull null
+                group.value.toString().run { if (isEmpty()) path.value else "$this/${path.value}" }
             }
+        }
     }
 
-    private fun matchRoutePath(element: PsiElement): String? {
-        if (element is KtElement) return matchRoutePath(element as KtElement)
-        if (element !is PsiCallExpression) return null
-        val method = element.resolveMethod() ?: return null
-        val parent = method.parent
-        if (method.name != "build" || parent !is PsiClass) return null
-        if (parent.name != SDK_NAME && parent.supers.find { it.name != SDK_NAME } != null) return null
-        val args = element.argumentList?.expressions
-        if (args.isNullOrEmpty()) return null
-        return (args[0] as? PsiLiteralValue)?.value?.toString() ?: return null
-    }
-
-    private fun matchRoutePath(element: KtElement): String? {
-        if (element !is KtNameReferenceExpression) return null
-        if (element.getReferencedName() != "build") return null
-        val parent = (element as PsiElement).parent
-        if (parent !is KtCallExpression) return null
-        val args = parent.valueArguments
-        if (args.isNullOrEmpty()) return null
-        return (args[0].getArgumentExpression() as? KtStringTemplateExpression)?.plainContent ?: return null
-    }
+    private fun matchRoutePath(element: PsiElement) =
+        if (element !is KtElement) element.buildArgs()?.getOrNull(0)?.cast<PsiLiteralValue>()?.value?.toString()
+        else (element as KtElement).buildArgs()?.getOrNull(0)
+            ?.getArgumentExpression()
+            ?.cast<KtStringTemplateExpression>()
+            ?.plainContent
 
     private fun markerInfo(
         element: PsiElement,
